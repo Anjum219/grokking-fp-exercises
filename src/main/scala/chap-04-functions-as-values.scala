39279dbc2ee49627ee1e9d151be4a76f657cdaf1
Anjum219 <anjumhaz@gmail.com>
2025-01-02 10:21:40 +0600

    Add filter exercises

--- src/main/scala/chap-04-functions-as-values.scala
+++ src/main/scala/chap-04-functions-as-values.scala
@@ -24,6 +24,29 @@ def sortByLetterSCountDesc(list: List[String]): List[String] =
   list.sortBy(_.count(_ == 's') * -1)
 
 
+// Practicing filter
+
+// Return words that are shorter than five characters.
+// input: List("scala", "rust", "ada") output: List("rust", "ada")
+def filterShorts(list: List[String]): List[String] =
+  list.filter(_.length < 5)
+
+// Return words that have more than two of the letter 's'.
+// input: List("rust", "ada") output: List()
+def filterMore(list: List[String]): List[String] =
+  list.filter(_.count(_ == 's') > 2)
+
+// Return a new List with only odd numbers.
+// input: List(5, 1, 2, 4, 0) output: List(5, 1)
+def filterOdd(list: List[Int]): List[Int] =
+  list.filter(_ % 2 == 1)
+
+// Return a new List with all numbers larger than 4.
+// input: List(5, 1, 2, 4, 0) output: List(5)
+def filterLarge(list: List[Int]): List[Int] =
+  list.filter(_ > 4)
+
+
 object chap04 extends App {
   assert(sortByLength(List("abc", "ac", "ab", "b", "a")) == List("b", "a", "ac", "ab", "abc"))
 
@@ -32,4 +55,12 @@ object chap04 extends App {
   assert(sortIntsDesc(List(1, 2, 3, 4, 5)) == List(5, 4, 3, 2, 1))
 
   assert(sortByLetterSCountDesc(List("ssss", "qwe", "zxc", "asd", "")) == List("ssss", "asd", "qwe", "zxc", ""))
+
+  assert(filterShorts(List("scala", "rust", "ada")) == List("rust", "ada"))
+
+  assert(filterMore(List("scala", "rust", "ada")) == List())
+
+  assert(filterOdd(List(5, 1, 2, 4, 0)) == List(5, 1))
+
+  assert(filterLarge(List(5, 1, 2, 4, 0)) == List(5))
 }
