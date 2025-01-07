bcc5ebc8e375ea7553502c8c65bc0e44c6842472
Anjum219 <anjumhaz@gmail.com>
2025-01-07 10:19:05 +0600

    Add returning function examples

--- src/main/scala/chap-04-functions-as-values.scala
+++ src/main/scala/chap-04-functions-as-values.scala
@@ -47,6 +47,42 @@ def filterLarge(list: List[Int]): List[Int] =
   list.filter(_ > 4)
 
 
+// Returning functions practice
+
+// Return a new List with all numbers larger than 4.
+// input: List(5, 1, 2, 4, 0) output: List(5)
+// Change: Now return a new List with all numbers larger than 1.
+// input: List(5, 1, 2, 4, 0) output: List(5, 2, 4)
+def largerThan(n: Int): Int => Boolean = i => i > n
+def filterListLargerThan(list: List[Int], n: Int): List[Int] =
+  list.filter(largerThan(n))
+
+// Return a new List that contains only numbers divisible by 5.
+// input: List(5, 1, 2, 4, 15) output: List(5, 15)
+// Change: Now return a new List that contains only number divisible by 2.
+// input: List(5, 1, 2, 4, 15) output: List(2, 4)
+def divisibleBy(n: Int): Int => Boolean = i => i % n == 0
+def filterListDivisibleBy(list: List[Int], n: Int): List[Int] =
+  list.filter(divisibleBy(n))
+
+// Return words that are shorter than four characters.
+// input: List("scala", "ada") output: List("ada")
+// Change: Now return words that are shorter than seven characters.
+// input: List("scala", "ada") output: List("scala", "ada")
+def shorterThan(threshold: Int): String => Boolean = str => str.length < threshold
+def filterListShorterThan(list: List[String], threshold: Int): List[String] =
+  list.filter(shorterThan(threshold))
+
+// Return words that have more than two of the letter 's' inside.
+// input: List("rust", "ada") output: List()
+// Change: Now return words that have more than zero of the letter 's'
+// inside.
+// input: List("rust", "ada") output: List("rust")
+def filterMoreThan(threshold: Int): String => Boolean = str => str.count(_ == 's') > threshold
+def filterListMoreThan(list: List[String], threshold: Int): List[String] =
+  list.filter(filterMoreThan(threshold))
+
+
 object chap04 extends App {
   assert(sortByLength(List("abc", "ac", "ab", "b", "a")) == List("b", "a", "ac", "ab", "abc"))
 
@@ -63,4 +99,16 @@ object chap04 extends App {
   assert(filterOdd(List(5, 1, 2, 4, 0)) == List(5, 1))
 
   assert(filterLarge(List(5, 1, 2, 4, 0)) == List(5))
+
+  assert(filterListLargerThan(List(5, 1, 2, 4, 0), 4) == List(5))
+  assert(filterListLargerThan(List(5, 1, 2, 4, 0), 1) == List(5, 2, 4))
+
+  assert(filterListDivisibleBy(List(5, 1, 2, 4, 15), 5) == List(5, 15))
+  assert(filterListDivisibleBy(List(5, 1, 2, 4, 15), 2) == List(2, 4))
+
+  assert(filterListShorterThan(List("scala", "ada"), 4) == List("ada"))
+  assert(filterListShorterThan(List("scala", "ada"), 7) == List("scala", "ada"))
+
+  assert(filterListMoreThan(List("rust", "ada"), 2) == List())
+  assert(filterListMoreThan(List("rust", "ada"), 0) == List("rust"))
 }
